866bd6380f5e1be4eb9083aec175d540afc8b7d0
zsatriawanz <satriawanstudy@gmail.com>
2026-03-23 13:35:01 +0800

    Mnemabhakan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
 	<h1>Hello Word!</h1>
+	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas accusamus soluta magnam dicta dolorum, optio tempore facere nam, vel, expedita deleniti vero rerum tempora quidem, eveniet? Enim, reprehenderit ex in!</p>
 </body>
 </html>
