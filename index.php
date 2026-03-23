25ac984c7f725be96404d53c520f9e3801d05ebd
zsatriawanz <satriawanstudy@gmail.com>
2026-03-23 13:42:04 +0800

    Menmabbkan list

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
 	<h1>Hello Word!</h1>
 	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas accusamus soluta magnam dicta dolorum, optio tempore facere nam, vel, expedita deleniti vero rerum tempora quidem, eveniet? Enim, reprehenderit ex in!</p>
+	<ul><li>Lorem.</li></ul>
 </body>
 </html>
